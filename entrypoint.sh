50cdf323aec9a5aaeb9edb427129ca7044be6511
Elias DeFaria <eliasdefaria.45@gmail.com>
2024-03-10 19:02:56 -0700

    Updated SDF version

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,6 @@ check_exit_status() {
     exit_status=$1
     command_log=$2
     echo "checking exit status: $exit_status"
-    echo "COMMAND_LOG_3000=$command_log"
     if [ $exit_status -ne 0 ]; then
         echo "$command_log"
         # Log the error message to GitHub output
@@ -63,12 +62,8 @@ fi
 
 # run and save outputs
 echo "running command: $input_command"
-$input_command
 log=$($input_command 2>&1)
-echo "EXIT_STATUS_1=$?"
-exit_status=$?
-echo "EXIT_STATUS_2=$exit_status"
-check_exit_status $exit_status "$log"
+check_exit_status $? "$log"
 
 echo "$log"
 {
