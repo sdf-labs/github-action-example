63ecfb12c58b91e197f6e91da2a2d1d83ca151b4
Elias DeFaria <eliasdefaria.45@gmail.com>
2024-03-10 17:38:22 -0700

    Testing action failure

--- ddls/payment/public/invoices.sql
+++ ddls/payment/public/invoices.sql
@@ -4,7 +4,6 @@ CREATE TABLE invoices (
     amount DECIMAL(10, 2),
     issued_date TIMESTAMP,
     due_date TIMESTAMP,
-    payer_user_id INT,
     paid_date TIMESTAMP,
     status VARCHAR(50),
     FOREIGN KEY (organization_id) REFERENCES organizations(organization_id),
