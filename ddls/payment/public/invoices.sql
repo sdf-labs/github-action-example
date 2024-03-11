09b667f068e187050cb71ed2f7ad1e867fa25c77
Elias DeFaria <eliasdefaria.45@gmail.com>
2024-03-10 19:05:27 -0700

    Correcting workspace for proper compilation

--- ddls/payment/public/invoices.sql
+++ ddls/payment/public/invoices.sql
@@ -4,6 +4,7 @@ CREATE TABLE invoices (
     amount DECIMAL(10, 2),
     issued_date TIMESTAMP,
     due_date TIMESTAMP,
+    payer_user_id INT,
     paid_date TIMESTAMP,
     status VARCHAR(50),
     FOREIGN KEY (organization_id) REFERENCES organizations(organization_id),
